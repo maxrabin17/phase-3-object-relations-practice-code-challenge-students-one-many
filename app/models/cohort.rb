ebda6055d5242deb607a1ecd58aa6c353964a094
maxrabin <max.rabin17@gmail.com>
2021-07-25 22:23:10 -0400

    Completed

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,3 +1,28 @@
 class Cohort < ActiveRecord::Base
-  
+    has_many :students
+
+    def add_student(name, age)
+        student = Student.create(name: name, age: age)
+        student.cohort_id = self.id
+        student.save
+    end
+
+    def average_age
+        self.students.average(:age).to_f
+    end
+
+    def total_students
+        self.students.count
+    end
+
+    def self.biggest
+        array = self.all.map {|c| c.students.count}
+        idx = array.index(array.max())
+        cohort = self.all[idx]
+        cohort
+    end
+
+    def self.sort_by_mod
+        self.order(:current_mod)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,28 +5,30 @@ Student.destroy_all
 # This will create 5 cohorts using a random date
 puts "Seeding cohorts..."
 c1 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
+  name: Faker::Name.last_name,
   current_mod: rand(1..5)
 )
 c2 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
+  name: Faker::Name.last_name,
   current_mod: rand(1..5)
 )
 c3 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
+  name: Faker::Name.last_name,
   current_mod: rand(1..5)
 )
 c4 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
+  name: Faker::Name.last_name,
   current_mod: rand(1..5)
 )
 c5 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
+  name: Faker::Name.last_name,
   current_mod: rand(1..5)
 )
 
 puts "Seeding students..."
 # TODO: create seed data for students
 # check your schema and make sure to create students with all the necessary data
-
+  10.times do
+    Student.create(name: Faker::Name.first_name, age: rand(18..99), cohort_id: rand(1..5))
+  end
 puts "Done!"
